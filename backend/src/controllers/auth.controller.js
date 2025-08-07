c37d867810efda0ad39db99ad33532e56803448e
BIPLABx019 <biplabx019@gmail.com>
2025-08-07 18:30:47 +0530

    feat: implement user authentication with JWT and bcrypt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,106 @@
-export function signup(req,res) {
-    res.send("Signup Route")
+import User from "../models/User.js"
+import jwt from "jsonwebtoken"
+
+
+export async function signup(req,res) {
+    const {email,password,userName,fullName} = req.body;
+
+    try {
+        if (!email || !password || !userName || !fullName){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        if (password.length < 6){
+            return res.status(400).json({message:"Password too short"});
+        }
+
+        const existingUserEmail = await User.findOne({email});
+        const existingUserName = await User.findOne({userName});
+
+        if (existingUserEmail){
+            return res.status(400).json({message:"Email already registered"});
+        }
+        if (existingUserName){
+            return res.status(400).json({message:"Username already exists"});
+        }
+
+        const index = Math.floor(Math.random() * 100) + 1;
+        const randomAvatar = `https://avatar-placeholder.iran.liara.run/public/${index}.png`
+
+        const newUser = await User.create({
+            userName,
+            fullName,
+            email,
+            password,
+            profilePic : randomAvatar
+        })
+
+        const token = jwt.sign({userID:newUser._id},process.env.JWT_SECRET_KEY,{
+            expiresIn: "7d"
+        })
+        
+        res.cookie("jwt", token,{
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.status(201).json({success:true, user:newUser})
+    } catch (error) {
+        console.log("Error in Signup",error)
+        return res.status(500).json({message: `${error}`});
+    }
 }
 
-export function login(req,res) {
-    res.send("login Route")
+export async function login(req,res) {
+    const {userNameOrEmail , password} = req.body;
+
+    try {
+        if (!userNameOrEmail || !password) {
+             return res.status(400).json({message:"All fields are required"});
+        }
+
+        const userByUserName = await User.findOne({userName: userNameOrEmail});
+        const userByEmail = await User.findOne({email: userNameOrEmail});
+
+        if (!userByUserName && !userByEmail) {
+            return res.status(401).json({message:"Invalid email or username"});
+        }
+
+        let user;
+
+        if (userByUserName) {
+            user = userByUserName;
+        }
+        else {
+            user = userByEmail;
+        }
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if (!isPasswordCorrect) return res.status(401).json({message:"Invalid password"});
+
+        const token = jwt.sign({userID:user._id},process.env.JWT_SECRET_KEY,{
+            expiresIn: "7d"
+        })
+        
+        res.cookie("jwt", token,{
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.status(200).json({success:true, user});
+
+    } catch (error) {
+        console.log("Error in Login",error)
+        return res.status(500).json({message: `${error}`});
+    }
+
 }
 
-export function logout(req,res) {
-    res.send("logout Route")
+export async function logout(req,res) {
+    res.clearCookie("jwt")
+    res.status(200).json({success: true, message: "User logged out successfully"})
 }
---
+++ backend/src/models/User.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose"
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  bio: {
+    type: String,
+    maxlength: 100,
+    default: ""
+  },
+  profilePic: {
+    type: String,
+    default: "" 
+  },
+  isOnboarded: {
+    type: Boolean,
+    deafault: false
+  },
+  friends: [{
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User"
+}]
+
+}, { timestamps: true });
+
+userSchema.pre("save",async function(next) {
+
+    if (!this.isModified("password")) return next();
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    }catch (err) {
+        next(err);
+    }
+})
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    const isPasswordCorrect = await bcrypt.compare(enteredPassword,this.password);
+    return isPasswordCorrect;
+}
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
